7ff92ffa94fec3af30be9266ed8c1a778736a5a0
martijnschermers <martijnschermers2@gmail.com>
2024-03-11 13:16:44 +0100

    feat: add report classes

---
+++ Domain/Report/ReportFooter.cs
@@ -0,0 +1,15 @@
+namespace Report
+{
+  public class ReportFooter(string companyName, string companyLogo, string projectName, string version, DateTime date) : ReportComponent(companyName, companyLogo, projectName, version, date)
+  {
+    public new void Print()
+    {
+      Console.WriteLine("Report Footer");
+      Console.WriteLine("Company Name: " + companyName);
+      Console.WriteLine("Company Logo: " + companyLogo);
+      Console.WriteLine("Project Name: " + projectName);
+      Console.WriteLine("Version: " + version);
+      Console.WriteLine("Date: " + date);
+    }
+  }
+}
