2265b956f71d8c36be235ea5e21db9865f66ef74
Luca.R04 <Lucarinck04@gmail.com>
2024-03-22 13:50:01 +0100

    remove comments

--- Domain/Users/UserContext.cs
+++ Domain/Users/UserContext.cs
@@ -7,14 +7,11 @@
         public UserContext()
         { }
 
-        // Usually, the Context accepts a strategy through the constructor, but
-        // also provides a setter to change it at runtime.
         public UserContext(IUserStrategy strategy)
         {
             this._strategy = strategy;
         }
 
-        // Usually, the Context allows replacing a Strategy object at runtime.
         public void SetStrategy(IUserStrategy strategy)
         {
             this._strategy = strategy;
